d298680b65fea173ae33f9f78093925a831b97b3
Derickpambah <pambahderick412@gmail.com>
2022-05-29 21:25:59 +0300

    Day 1 of 100
    Handle 'let' and 'var' keywords

---
+++ hello.js
@@ -0,0 +1,10 @@
+// Initializing variables using var
+var a = 5;
+var myFirstName = "Derick";
+
+// Initializing variables using let
+let b = 35;
+let myLastName = "Pambah";
+
+// Print 'Hello, World!
+console.log("Hello, World!");
