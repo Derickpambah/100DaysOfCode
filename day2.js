aaad6704774eab703660608022111042a7e46f84
Derickpambah <pambahderick412@gmail.com>
2022-05-30 17:39:40 +0300

    Day 2 of 100
    Manipulate strings and Arrays(declaration and append methods)

---
+++ day2.js
@@ -0,0 +1,11 @@
+/* Day2 of 100
+   - Strings: concatenation and escape sequences
+   - Arrays: push(), pop(), shift(), and unshift()
+*/
+
+let names = [["Derick", "Briton"], ["Keiz", "Keira"]];
+console.log(names[0][1]);
+
+let firstName = "Chris";
+let fullName = firstName += " Hemsworth";
+console.log(fullName);
