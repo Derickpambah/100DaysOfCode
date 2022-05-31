c1927a2a0808270941faed918a61f85fc5b39028
Derickpambah <pambahderick412@gmail.com>
2022-05-31 22:46:06 +0300

    Day 3 of 100
    Learn functions | Conditional logic | Comparison operators

---
+++ day3.js
@@ -0,0 +1,20 @@
+/* Day 3 of 100
+   - Handled JS functions
+   - Differentiated between local and global scope
+   - Return statement
+   - Conditional Logic with IF statements
+   - Comparison with equality, inequality, and greater than operators
+*/
+
+function addition(num1, num2){
+    return num1 + num2;
+}
+console.log(addition(8, 3));
+
+let myFirstName = "Shawn";
+let myLastName = "Matrix";
+if (myFirstName.length == myLastName.length){
+    result = true;
+}
+result = false;
+console.log(result);
