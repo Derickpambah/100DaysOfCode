4dd10494a4f8b30eab10c9a3b3ccb233c4d1ffdc
Derickpambah <pambahderick412@gmail.com>
2022-06-07 23:38:44 +0300

    Day 10 of 100
    Tackle While Loops

---
+++ day10.js
@@ -0,0 +1,15 @@
+/**
+ * Day 10 of 100
+ *  - Completed the first module under the FreeCodeCamp JS Course
+ *  - Tackled while loops
+ */
+
+const myArray = [];
+
+let i = 0;
+
+while (i < 5){
+    myArray.push(i);
+    i++;
+}
+console.log(myArray)
